747dce067040bed72f56bf49896ea8b09a970fc4
Sadık Sünbül <109942759+SadikSunbul@users.noreply.github.com>
2024-06-13 22:42:38 +0300

    Graf temelleri yazıldı

---
+++ veriyapilari/Graflar/graf.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func main() {
+	GrafıBaşlat(5)
+	BağlantıOluştur(1, 2)
+	BağlantıOluştur(2, 2)
+	BağlantıOluştur(3, 2)
+	BağlantıOluştur(3, 3)
+	BağlantıOluştur(3, 4)
+	BağlantıKaldır(3, 4)
+	fmt.Print()
+}
+
+var Graf [][]int
+
+/* Komsuluk matrısı ıle yapıcaz işlemlerimizi
+	  1	 2  3  4
+    1
+    2
+	3
+	4
+*/
+
+func GrafıBaşlat(dugumSayisi int) {
+	Graf = make([][]int, dugumSayisi)
+	for i := range Graf {
+		Graf[i] = make([]int, dugumSayisi)
+	}
+	//GrafıBaşlat fonksiyonunda Graf değişkenine boyut atanıyor,
+	//ancak her bir satırın boyutu atanmıyor. GrafıBaşlat fonksiyonunda,
+	//her bir satır için slice oluşturmanız gerekir.
+}
+
+func BağlantıOluştur(aKonumundan, bKonumuna int) {
+	Graf[aKonumundan][bKonumuna] = 1
+}
+
+func BağlantıKaldır(aKonumundan, bKonumuna int) {
+	Graf[aKonumundan][bKonumuna] = 0
+}
